9a70abafaf799bf3c2485b516ee01a0b549769b7
doctor-rutvik14 <rutvik14d@gmail.com>
2020-08-10 10:44:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{useState, Button, Card}from 'react';
 import './App.css';
+import './index.css'
 
 function App() {
+  let t = new Date().toLocaleTimeString();
+  const [tym,setTym] = useState(t);  
+  const Show = () => {
+    t = new Date().toLocaleTimeString();
+    setTym(t);
+  }
+  setInterval(Show,1000);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{tym}</h1>
     </div>
   );
 }
-
 export default App;
+
